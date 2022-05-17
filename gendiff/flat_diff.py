f46743fd3303d6fe5d872214a4678499f645e100
devdenh <denisaminov74@gmail.com>
2022-05-17 10:53:06 +0500

    step3 linterr refactor

--- gendiff/flat_diff.py
+++ gendiff/flat_diff.py
@@ -4,8 +4,10 @@ import os
 
 def generate_diff_(file_path1, file_path2):
 
-    with open(os.path.abspath(file_path1)) as file1,\
-            open(os.path.abspath(file_path2)) as file2:
+    abs_path1 = os.path.join(os.getcwd(), file_path1)
+    abs_path2 = os.path.join(os.getcwd(), file_path2)
+
+    with open(abs_path1) as file1, open(abs_path2) as file2:
         data1, data2 = json.load(file1), json.load(file2)
 
         for item in data1:
@@ -15,18 +17,21 @@ def generate_diff_(file_path1, file_path2):
         for _item in data2:
             if isinstance(data2[_item], bool):
                 data2[_item] = json.dumps(data2[_item])
+    return flatt_differ(data1, data2)
 
-        result = '{\n'
-        for key in sorted(data1.keys() | data2.keys()):
-            if key in data1 and key in data2:
-                if data1[key] == data2[key]:
-                    result += f'    {key}: {data1[key]}\n'
-                else:
-                    result += f'  - {key}: {data1[key]}\n'
-                    result += f'  + {key}: {data2[key]}\n'
-            elif key in data1 and key not in data2:
+
+def flatt_differ(data1, data2):
+    result = '{\n'
+    for key in sorted(data1.keys() | data2.keys()):
+        if key in data1 and key in data2:
+            if data1[key] == data2[key]:
+                result += f'    {key}: {data1[key]}\n'
+            else:
                 result += f'  - {key}: {data1[key]}\n'
-            elif key in data2 and key not in data1:
                 result += f'  + {key}: {data2[key]}\n'
-        result += '}'
-        return result
+        elif key in data1 and key not in data2:
+            result += f'  - {key}: {data1[key]}\n'
+        elif key in data2 and key not in data1:
+            result += f'  + {key}: {data2[key]}\n'
+    result += '}'
+    return result
