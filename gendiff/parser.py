a38a5c72a3e74ffe15378a00a703717b5fc4163d
devdenh <denisaminov74@gmail.com>
2022-05-24 09:18:23 +0500

    step6 trying build tree

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,13 +1,36 @@
 import json
 import yaml
+from tests.fixtures.expected import read
+from tests.fixtures.expected import get_fixture_path
+import hexlet.fs as fs
 
 
 def parser(data, type):
     if type == 'json':
-        data = json.load(data)
+        data = json.loads(data)
         for item in data:
             if isinstance(data[item], bool):
                 data[item] = json.dumps(data[item])
+            elif isinstance(data[item], dict):
+                return recursive_parser(data)
     elif type == 'yaml':
         data = yaml.load(data, Loader=yaml.BaseLoader)
     return data
+
+
+def recursive_parser(data):
+    for item in data:
+        if isinstance(data[item], bool):
+            data[item] = json.dumps(data[item])
+        elif isinstance(data[item], dict):
+            recursive_parser(data[item])
+    return data
+
+
+data1 = (parser(read(get_fixture_path('nested_file1.json')), 'json'))
+data2 = (yaml.load(read(get_fixture_path('nested_file2.yml')), Loader=yaml.BaseLoader))
+
+
+# print(data1)
+# print(data2)
+
--- tests/fixtures/expected.py
+++ tests/fixtures/expected.py
@@ -1,3 +1,20 @@
+import os
+
+
+def read(file_path):
+    with open(file_path, 'r') as f:
+        result = f.read()
+    return result
+
+
+def get_fixture_path(file_name):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, file_name)
+
+
+nested_data = read(get_fixture_path('nested.txt')).rstrip().split('\n\n\n')
+
+
 def expected_for_flat_diff_json():
     start = '{'
     end = '}'
@@ -9,3 +26,8 @@ def expected_for_flat_diff_json():
                 f'+ timeout: 20\n  '
                 f'+ verbose: true\n'
                 f'{end}')
+
+
+def expected_for_nested_diff_json():
+    return nested_data
+
