11bdd2669e528310f5d38b30f4f1ab6f19bb6a9e
devdenh <denisaminov74@gmail.com>
2022-06-01 15:48:39 +0500

    final tests fix init

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,5 +1,5 @@
 from gendiff.diff_generator import generate_diff_
 
 
-def generate_diff(path1, path2, format_name):
+def generate_diff(path1, path2, format_name=None):
     return generate_diff_(path1, path2, format_name)
