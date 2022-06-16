2fc0a7d79342defad011af2be5176f51bf52ff88
devdenh <denisaminov74@gmail.com>
2022-06-16 16:53:36 +0500

    refactor #2 debug err in function

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -7,7 +7,6 @@ from gendiff.formatter import format
 def generate_diff(file_path1, file_path2, format_name='stylish'):
     paths = (file_path1, file_path2)
     abs_paths = tuple(map(os.path.abspath, paths))
-    print(abs_paths)
     data1, data2 = get_data(abs_paths[0]), get_data(abs_paths[1])
     formated_data1 = parse(data1, get_format(paths[0]))
     formated_data2 = parse(data2, get_format(paths[1]))
